f15d0663e0c32eff46096fb5d11819712528c277
hkjoker23 <hkjoker23@gmail.com>
2021-01-22 15:45:21 +0900

    Init Project

---
+++ accountapp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from accountapp.views import hello_world
+
+app_name = "accountapp"
+
+urlpatterns = [
+    path('helloworld/', hello_world, name="accountapp"),
+]
