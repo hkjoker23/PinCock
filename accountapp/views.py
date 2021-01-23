05edddb3829d00208c659778e922e9e576d25124
hkjoker23 <hkjoker23@gmail.com>
2021-01-23 23:01:39 +0900

    test function

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,12 @@
+from django import http
 from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from accountapp.models import HelloWorld
+
 # Create your views here.
+
+# test view.function
 
 
 def hello_world(request):
